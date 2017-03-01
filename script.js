60417c03e29a0e0175d5bf96b97f76efe874ab2a
Upser <bashkirovevgene@gmail.com>
2017-03-01 17:24:34 +0300

    2017.03.01

--- script.js
+++ script.js
@@ -10,44 +10,8 @@ function openMenu()
 	}
 }
 
-function fib(n)
-{
-	for (var i = n; i>=1;i--)
-	{
-
-		if (i!=1 && i!=2)
-		{
-			var x = (fib(i-1)+fib(i-2));
-			var y = x +" ,";
-			return y;
-		}else{
-			return 1;
-		}
-	}	
-}
-
-alert (fib(6) );
-
-/*https://github.com/EBV1987/first_project*/
-
-/*function sayHi() {
-  alert( "Привет" );
-}
-var func = sayHi;    // (2)
-func(); // Привет    // (3)
-*/
-/*
-sayHi = null;
-sayHi();             // ошибка (4)
-*/
-
-//alert( sayHi ); // выведет код функции	
 
 
-/*var years = confirm("How old are you?");
-alert( "You are so old - " + years );
-*/
 
-//alert( alert(1) || 2 || alert(3) ); // ????
-//console.log( console.log(1) || 2 || console.log(3) );
+
 //https://github.com/EBV1987/first_project
